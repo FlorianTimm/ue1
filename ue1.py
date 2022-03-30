e3ce2476367166d997a94ef91f7a61b08746c20d
grndng <gueren.dinga@hcu-hamburg.de>
2022-03-30 14:08:29 +0200

    Gute commit messages sind wichtig ;)

---
+++ ue1.py
@@ -0,0 +1,7 @@
+#DataMod UE1 'Git'
+#Bitte arbeitet eure Namen in einem
+#separaten branch ein und führt diesen
+#dann im master zusammen!
+#bei Fragen: gueren.dinga@hcu-hamburg.de
+
+print('Hello, my name is Güren')
